aa55ce5952b68d469dc9349468a2e2d0a242aca9
Daniel Valchev <dvalchevvv@gmail.com>
2024-08-09 16:39:08 +0300

    Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 import Campaigns from "./pages/Campaigns";
+import Footer from "./pages/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <HomePage />
       <Campaigns />
+      <Footer />
     </>
   );
 }
--- client/src/pages/Footer/Footer.tsx
+++ client/src/pages/Footer/Footer.tsx
@@ -1,5 +1,63 @@
+import facebook from "@/assets/facebook.svg";
+import instagram from "@/assets/instagram.svg";
+import linkedin from '@/assets/linkedin.svg';
+import x from "@/assets/x.svg"
+import tiktok from '@/assets/tiktok.svg';
+
+
+const FooterSection = ({ title, items }: { title: string, items: string[] }) => {
+  return (
+    <div className="w-60">
+      <h4 className='text-xl font-bold'>{title}</h4>
+      <nav>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index} className="hover:font-bold cursor-pointer py-2">{item}</li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
 const Footer = () => {
-  return <div>asd</div>;
+  const sections = [
+    { title: 'Yes', items: ['Contacts', 'About us', 'Blog'] },
+    { title: 'Campaigns', items: ['Last News', 'Top Campaigns', 'New Campaigns', 'Best Campaigns1'] },
+    { title: 'Information', items: ['Help Center', 'How it works', 'Taxes', 'Private Info', 'Cookies'] }
+  ];
+
+  return (
+    <footer className="px-10 py-8 bg-[#EDEFFC] rounded-md">
+      <div className="flex w-full gap-9">
+        {sections.map((section, index) => (
+          <FooterSection key={index} title={section.title} items={section.items} />
+        ))}
+        <div className="w-60">
+          <h4 className='text-xl font-bold'>Social Media</h4>
+          <nav >
+            <ul className="flex items-center gap-3">
+              <li className="hover:font-bold cursor-pointer py-2">
+                <img src={facebook} />
+              </li>
+              <li>
+                <img src={instagram} />
+              </li>
+              <li>
+                <img src={linkedin} />
+              </li>
+              <li>
+                <img src={tiktok} />
+              </li>
+              <li>
+                <img src={x} />
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
