525a4ccd9621128869c4cb142d9ca3609826b6a6
Daniel Valchev <dvalchevvv@gmail.com>
2024-08-13 07:33:28 +0300

    Update wagmi.ts

--- client/src/wagmi.ts
+++ client/src/wagmi.ts
@@ -1,7 +1,7 @@
 import { http, createConfig } from "@wagmi/core";
 import { sepolia } from "@wagmi/core/chains";
 
-const alchemyApiKey = import.meta.env.VITE_WC_ALCHEMY_API_KEY;
+// const alchemyApiKey = import.meta.env.VITE_WC_ALCHEMY_API_KEY;
 
 export const config = createConfig({
   chains: [sepolia],
